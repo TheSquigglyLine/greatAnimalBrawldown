ec7e329e2625c3d4c06bd34d0a496d4280881e2e
Peter May <peter.n.may@gmail.com>
2023-02-21 16:38:51 -0600

    more pathing

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'), function(err) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
