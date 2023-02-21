322310084022d3ec5fc0fe01d51a6337cff0978c
Peter May <peter.n.may@gmail.com>
2023-02-21 16:44:23 -0600

    one last fix maybe

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static('build')); // serve static files (css & js) from the 'pub
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('*', function(req, res) {
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
