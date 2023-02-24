260ec9479f6531a94b22aadf8ffa520e4fdcdaf1
Peter May <peter.n.may@gmail.com>
2023-02-23 22:05:01 -0600

    quick https fix

--- app.js
+++ app.js
@@ -38,6 +38,6 @@ app.get('/*', function(req, res) {
     }
   })
 })
-http.createServer(app).listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(PORT, function() {
+    console.log('Express server listening on port ' + PORT);
 });
